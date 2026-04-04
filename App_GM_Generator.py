3405299d6ed3b025c98071b7c2af39e65dfaf9e9
Victor1492Alvarez <victorsamuel1492@gmail.com>
2026-04-04 16:30:01 +0200

    Update consolidated error chart height to 9.0 cm

--- App_GM_Generator.py
+++ App_GM_Generator.py
@@ -59,7 +59,7 @@ PDF_SETTINGS = {
     "training_prediction_chart_height_cm": 9.0,
     "training_error_chart_height_cm": 8.0,
     "consolidated_main_chart_height_cm": 8.7,
-    "consolidated_error_chart_height_cm": 8.0,
+    "consolidated_error_chart_height_cm": 9.0,
 }
 
 plt.rcParams.update(
