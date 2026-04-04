76446cde2030956679a0d7a67edc7aaf09350f59
Victor1492Alvarez <victorsamuel1492@gmail.com>
2026-04-04 13:06:09 +0200

    Update App_GM_Generator.py

--- App_GM_Generator.py
+++ App_GM_Generator.py
@@ -38,15 +38,15 @@ RANDOM_STATE = 42
 # ------------------------------------------------------------------
 PLOT_SETTINGS = {
     "prediction_figsize": (8.8, 8.8),
-    "percent_error_figsize": (12.0, 5),
+    "percent_error_figsize": (12.0, 5.0),
     "cv_figsize": (8.8, 5.0),
     "external_comparison_figsize": (8.8, 5.0),
     "external_error_figsize": (12.0, 5.0),
     "title_fontsize": 10,
     "axis_label_fontsize": 9,
     "tick_x_fontsize": 6,
-    "tick_y_fontsize": 8,
-    "legend_fontsize": 8,
+    "tick_y_fontsize": 10,
+    "legend_fontsize": 10,
 }
 
 PDF_SETTINGS = {
