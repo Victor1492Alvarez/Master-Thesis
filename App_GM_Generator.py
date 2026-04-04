d0ece5e872937dd1129e4098c66ce562d1d4511e
Victor1492Alvarez <victorsamuel1492@gmail.com>
2026-04-04 14:16:40 +0200

    Increase training CV chart height from 9.0 to 12.0 cm

--- App_GM_Generator.py
+++ App_GM_Generator.py
@@ -58,7 +58,7 @@ PDF_SETTINGS = {
     "section_gap_cm": 3.0,
     "small_gap_cm": 3.0,
     "table_chart_gap_cm": 3.0,
-    "training_cv_chart_height_cm": 9.0,
+    "training_cv_chart_height_cm": 12.0,
     "training_prediction_chart_height_cm": 7.9,
     "training_error_chart_height_cm": 5.8,
     "consolidated_main_chart_height_cm": 8.8,
