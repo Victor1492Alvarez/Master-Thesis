32b6d08819e38c3b91a148719fba4b532796ea61
Victor1492Alvarez <victorsamuel1492@gmail.com>
2026-04-06 12:59:26 +0200

    Update App_GM_Generator.py

--- App_GM_Generator.py
+++ App_GM_Generator.py
@@ -1036,7 +1036,7 @@ def create_external_comparison_plot(df_plot: pd.DataFrame, input_col: str, outpu
         ordered[input_col],
         ordered[output_col],
         color="black",
-        s=7,
+        s=10,
         zorder=4,
         label="Aspen Simulation",
     )
